de22d5df0c2451cedb684cd4c744d0c0de87bf4e
Aayush Sharma <aayushdherad@gmail.com>
2023-01-13 19:20:29 +0530

    Spaces

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.17;
 
 contract Bank{
+    
     mapping(address => uint) private account_balance;
     mapping(address => uint[]) private blocks;
+
     function deposit_funds() public payable{
         account_balance[msg.sender] += msg.value;
         blocks[msg.sender].push(block.number);
     }
+
     function withdraw_funds(uint _funds) public {    
         require(account_balance[msg.sender]>=_funds,"Insufficient Balance");
         account_balance[msg.sender]-= _funds;
@@ -15,6 +18,7 @@ contract Bank{
         require(success,"Insufficient Balance");
         blocks[msg.sender].push(block.number);
     }
+
     function transfer_funds(uint _funds, address payable rec) public {
         require(account_balance[msg.sender]>=_funds,"Insufficient Balance");
         account_balance[msg.sender]-= _funds;
@@ -22,9 +26,11 @@ contract Bank{
         blocks[msg.sender].push(block.number);
         blocks[rec].push(block.number);
     }
+
     function get_balance() public view returns(uint){
         return account_balance[msg.sender];
     }
+
     function get_blocks() public view returns(uint[] memory){
         return blocks[msg.sender];
     }
